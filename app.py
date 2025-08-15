327363fabcc6044560cc2c232a757d7312d93d66
hfariha <husnafariha1203@gmail.com>
2025-08-16 09:51:45 +1000

    sql

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') #master page
 
 if __name__ == "__main__":
     app.run(debug=True) 
