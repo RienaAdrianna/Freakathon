6f5a2ef302860c968b43d2b61ace21731ea08815
farahdroslan <farahdarwisyah@outlook.com>
2025-08-16 14:00:19 +1000

    app.py update

--- app.py
+++ app.py
@@ -4,20 +4,10 @@ from datetime import datetime
 
 app = Flask(__name__)
 # Add Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///grocery.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:grocery123@localhost/grocery'
 db = SQLAlchemy(app)
 
-# Create Category Model
-class Category(db.Model):
-    category_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    category_name = db.Column(db.String(200), nullable=False, unique=True)
-    description = db.Column(db.Text)
-
-    def __repr__(self):
-        return f'<Category {self.category_name}>'
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
     return render_template('dashboard.html') #master page
 
