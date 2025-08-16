50b9ba4408e213b730003c58c0cb5f18b6dae6d9
hfariha <husnafariha1203@gmail.com>
2025-08-16 17:34:26 +1000

    changes

--- app.py
+++ app.py
@@ -5,11 +5,16 @@ from datetime import datetime
 app = Flask(__name__)
 # Add Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:grocery123@localhost/grocery'
-db = SQLAlchemy(app)
+
 
 @app.route('/')
+@app.route('/dashboard')
+
+def dashboard():
+    return render_template('dashboard.html')
+
 def index():
-    return render_template('dashboard.html') #master page
+    return render_template('layout.html')  
 
 if __name__ == "__main__":
     app.run(debug=True) 
